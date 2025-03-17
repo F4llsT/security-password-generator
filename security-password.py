aeab3f069bcdf16251c49b9d5ce72c4176af5f2c
Arthur Lopes Libonati <arthurlibonati5@gmail.com>
2025-03-17 19:22:14 -0300

    Create security-password.py
    Importação da biblioteca de randomização, mensagem inicial e inserção de caracteres específicos

---
+++ security-password.py
@@ -0,0 +1,6 @@
+import random #importando a biblioteca de randomização
+
+print("Bem-vindo ao seu gerador de senha pessoal!") #título inicial
+
+caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$_&*()/1234567890" 
+#alfabeto maiúsculo e minúsculo, além de alguns símbolos e números para a randomização
