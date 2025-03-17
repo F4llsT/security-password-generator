49dc1411fa016def74308479c0a501cfbcc0fa8e
Arthur Lopes Libonati <arthurlibonati5@gmail.com>
2025-03-17 19:23:35 -0300

    Update security-password.py
    Adição de inputs e algumas notas

--- security-password.py
+++ security-password.py
@@ -4,3 +4,9 @@ print("Bem-vindo ao seu gerador de senha pessoal!") #título inicial
 
 caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$_&*()/1234567890" 
 #alfabeto maiúsculo e minúsculo, além de alguns símbolos e números para a randomização
+
+numero = int(input("Qual é o número de senhas que você deseja criar? "))
+quantidade = int(input("Qual é o número de caracteres que você deseja para cada senha? "))
+#requisição de valores sobre o número de senhas que o usuário deseja criar  
+#e o número de caracteres destas.
+
