2e00552dd191d3f2c70717e98bd28a28a21c6a5d
Arthur Lopes Libonati <arthurlibonati5@gmail.com>
2025-03-17 19:26:26 -0300

    Adicionando o output e o resultado da petição de senhas

--- security-password.py
+++ security-password.py
@@ -10,3 +10,9 @@ quantidade = int(input("Qual é o número de caracteres que você deseja para ca
 #requisição de valores sobre o número de senhas que o usuário deseja criar  
 #e o número de caracteres destas.
 
+print("\nAqui estão suas novas senhas:\n") #requisição de saída do código
+
+for _ in range(numero): 
+    senha = "".join(random.choice(caracteres) for _ in range(quantidade)) #Comando para randomizar entre todos os caracteres
+    print(senha) #saída resultante do código, com o número de senhas e caracteres requerido.
+
